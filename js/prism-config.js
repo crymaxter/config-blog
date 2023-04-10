4b7929e922481be3c0afa8c432d85c4debcd3336
crymaxter <130338136+crymaxter@users.noreply.github.com>
2023-04-09 23:28:19 -0600

    Add files via upload

---
+++ js/prism-config.js
@@ -0,0 +1,7 @@
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/prism.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-markup.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-css.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-javascript.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-python.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-yaml.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-docker.min.js"><\/script>');
